30c468d8132adbfb59ce2737e4f496d1b1510959
SarahHanks04 <sarahhanks04@gmail.com>
2024-07-24 00:47:11 +0100

    updated

--- src/component/Talent/Talent.jsx
+++ src/component/Talent/Talent.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Talent = () => {
   return (
     <section>
-      <h1 className="text-5xl font-semibold text-center py-[30px]">The Bulb Tech Talent Model</h1>
-      <div className=""></div>
+      <h1 className="text-5xl font-semibold text-center py-[30px] relative">
+        The Bulb Tech Talent Model
+      </h1>
+      <div className="w-1 h-[90%] bg-gray-300 flex justify-center align-middle text-center py-[20px]"></div>
+
+      {/* FIRST CIRCLE */}
+      <div class="flex items-center justify-center w-52 h-52 bg-blue-500 rounded-full">
+        <div class="w-24 h-24 bg-red-500 rounded-full"></div>
+        <div></div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Talent;
