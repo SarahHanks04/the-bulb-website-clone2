9bf7259287c538251c2aea8354ff20100c7a6b8e
SarahHanks04 <sarahhanks04@gmail.com>
2024-07-24 01:10:38 +0100

    updated

--- src/component/Talent/Talent.jsx
+++ src/component/Talent/Talent.jsx
@@ -3,16 +3,25 @@ import React from "react";
 const Talent = () => {
   return (
     <section>
-      <h1 className="text-5xl font-semibold text-center py-[30px] relative">
-        The Bulb Tech Talent Model
-      </h1>
-      <div className="w-1 h-[90%] bg-gray-300 flex justify-center align-middle text-center py-[20px]"></div>
+      <article>
+        <h1 className="text-5xl font-semibold text-center py-[30px] relative">
+          The Bulb Tech Talent Model
+        </h1>
 
-      {/* FIRST CIRCLE */}
-      <div class="flex items-center justify-center w-52 h-52 bg-blue-500 rounded-full">
-        <div class="w-24 h-24 bg-red-500 rounded-full"></div>
-        <div></div>
-      </div>
+        {/* THE TITLE */}
+        <div className="flex justify-between">
+          <h1 className="">For Talents</h1>
+          <h1>For Business</h1>
+        </div>
+        {/* THE LINE */}
+        <div className="w-1 h-[90%] bg-gray-300 absolute right-[50%] text-center py-[20px] mt-[10rem]"></div>
+
+        {/* FIRST CIRCLE */}
+        <div class="flex items-center justify-center w-5 h-5 bg-yellow-100 rounded-full">
+          <div class="w-3 h-3 bg-[#FDBF17] rounded-full"></div>
+          <div></div>
+        </div>
+      </article>
     </section>
   );
 };
