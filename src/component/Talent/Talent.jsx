e0d31f780d9b36b885dba4e49a9ab46398325983
SarahHanks04 <sarahhanks04@gmail.com>
2024-07-25 03:06:29 +0100

    updated

--- src/component/Talent/Talent.jsx
+++ src/component/Talent/Talent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Talent = () => {
   return (
-    <section className="">
+    <section className="h-screen">
       <article className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-5xl font-semibold text-center py-[30px] font-gothic">
           The Bulb Tech Talent Model
@@ -20,37 +20,64 @@ const Talent = () => {
           <div className="absolute inset-0 flex justify-center">
             <div className="w-1 bg-gray-300"></div>
           </div>
-          {/* <!-- Timeline Items 1 --> */}
+
+          {/* <!-- TIMELINE ITEM 1 --> */}
           <div classname="relative">
-            <div classname="absolute left-1/2 transform -translate-x-1/2 w-4 h-4 bg-yellow-500 rounded-full top-[30%]">
-              <div classname="absolute left-3/4 transform -translate-x-1/2 w-4 h-4 bg-[#FDBF17] rounded-full top-[30%]"></div>
+            {/* INNER LINE */}
+            <div className="absolute w-24 h-[2px] bg-[#FDBF17] right-[42%] top-[3.5%]"></div>
+            {/* INNER CIRCLE */}
+            <div className="absolute left-[49.5%] w-5 h-5 bg-yellow-100 rounded-full">
+              <div className="w-3 h-3 bg-[#FDBF17] rounded-full left-[50%]"></div>
             </div>
+            {/* INNER TEXT */}
             <div className="ml-[42rem] ">
-              <h3 className="text-lg font-bold">StartUp Academy</h3>
+              <h3 className="text-lg font-bold py-5">StartUp Academy</h3>
               <p>
-                We educate and offer training and mentorship to <br /> founders who are
-                at the beginning of their startup <br /> journey.
+                We educate and offer training and mentorship to <br /> founders
+                who are at the beginning of their startup <br /> journey.
               </p>
             </div>
           </div>
+
           {/* <!-- TIMELINE ITEM 2 --> */}
-          <div className="relative mb-10">
-                {/* INNER LINE */}
+          <div className="relative mb-5">
+            {/* INNER LINE */}
             <div className="absolute w-24 h-[2px] bg-gray-400 right-[50%] bottom-[94%]"></div>
-              {/* INNER CIRCLE */}
+            {/* INNER CIRCLE */}
             <div className="absolute left-[49.5%] w-5 h-5 bg-gray-200 rounded-full">
               <div className="w-3 h-3 bg-[#13162D] rounded-full left-[50%]"></div>
             </div>
-            <div className="ml-[10rem] mt-[10rem]">
+            {/* INNER TEXT */}
+            <div className="ml-[10rem] mt-[2rem]">
               <h3 className="text-lg font-bold py-5">Education</h3>
               <p>
-                Our commitment to sustainability shines through as <br /> we proudly
-                announce the generation of 5,000 <br /> megawatt-hours of renewable
-                energy, contributing <br /> to a greener and more environmentally friendly <br /> future.
+                Our commitment to sustainability shines through as <br /> we
+                proudly announce the generation of 5,000 <br /> megawatt-hours
+                of renewable energy, contributing <br /> to a greener and more
+                environmentally friendly <br /> future.
               </p>
             </div>
           </div>
-          {/* <!-- Add more timeline items as needed --> */}
+
+          {/* <!-- TIMELINE ITEM 3 --> */}
+          <div className="relative mb-10">
+            {/* INNER LINE */}
+            <div className="absolute w-24 h-[2px] bg-gray-400 right-[42%] bottom-[94%]"></div>
+            {/* INNER CIRCLE */}
+            <div className="absolute left-[49.5%] w-5 h-5 bg-yellow-100 rounded-full">
+              <div className="w-3 h-3 bg-[#FDBF17] rounded-full left-[50%]"></div>
+            </div>
+            {/* INNER TEXT */}
+            <div className="ml-[42rem] mt-[2rem]">
+              <h3 className="text-lg font-bold py-5">StartUp Engine</h3>
+              <p>
+                Our start up engine is geared towards incubating <br />{" "}
+                facinating ideas, accelerating early-stage startups, <br /> and
+                co-investing in startups that can drive growth <br /> and
+                innovation in Africa.
+              </p>
+            </div>
+          </div>
         </div>
 
         {/* THE LINE */}
