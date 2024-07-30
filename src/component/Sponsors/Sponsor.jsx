e56a614f72ce5dfb9973f87e0553de3d0c4db099
SarahHanks04 <sarahhanks04@gmail.com>
2024-07-30 21:58:48 +0100

    updated

--- src/component/Sponsors/Sponsor.jsx
+++ src/component/Sponsors/Sponsor.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+const images = [
+  "https://twl.techcabal.com/wp-content/uploads/2019/02/Lagos-Initiative.png",
+  "https://static-00.iconduck.com/assets.00/access-bank-plc-icon-512x128-r5rvcuv8.png",
+  "https://static-00.iconduck.com/assets.00/wema-bank-icon-2048x1148-78xxm99q.png",
+  "https://global.ariseplay.com/amg/www.thisdaylive.com/uploads/GetFundedAfrica.jpeg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUl-9r9mMim20lBDA0_Qh3eZz0h0UmVNzuwA&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxYvZANqq_87nliOnsi4b7ktTG1dkSJhrTfg&s"
+];
+
 const Sponsor = () => {
   return (
     <section>
@@ -10,33 +19,33 @@ const Sponsor = () => {
 
         <div className="flex">
           <img
-            src="https://twl.techcabal.com/wp-content/uploads/2019/02/Lagos-Initiative.png"
+            src=""
             alt=""
             width={300}
           />
           <img
-            src="https://static-00.iconduck.com/assets.00/access-bank-plc-icon-512x128-r5rvcuv8.png"
+            src=""
             alt=""
             width={250}
           />
           <img
-            src="https://static-00.iconduck.com/assets.00/wema-bank-icon-2048x1148-78xxm99q.png"
+            src=""
             alt=""
             width={250}
           />
           <img
-            src="https://global.ariseplay.com/amg/www.thisdaylive.com/uploads/GetFundedAfrica.jpeg"
+            src=""
             alt=""
             width={150}
             height={50}
           />
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUl-9r9mMim20lBDA0_Qh3eZz0h0UmVNzuwA&s"
+            src=""
             alt=""
             width={120}
           />
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxYvZANqq_87nliOnsi4b7ktTG1dkSJhrTfg&s"
+            src=""
             alt=""
             width={120}
           />
